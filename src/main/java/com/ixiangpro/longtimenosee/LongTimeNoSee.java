a87e4445054f9a695e36aa010f83f5361ed371ce
iXiangPro <132210248+XXJNOTFOUND@users.noreply.github.com>
2024-12-02 13:39:00 +0800

    Update LongTimeNoSee.java

--- src/main/java/com/ixiangpro/longtimenosee/LongTimeNoSee.java
+++ src/main/java/com/ixiangpro/longtimenosee/LongTimeNoSee.java
@@ -1,35 +1,30 @@
 package com.ixiangpro.longtimenosee;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LongTimeNoSee extends JavaPlugin {
+
+    private DatabaseManager databaseManager;
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        databaseManager = new DatabaseManager(this);
+        databaseManager.initializeDatabase();
 
-        // 检测 AuthMe 插件
-        if (getServer().getPluginManager().getPlugin("AuthMe") != null) {
-            getLogger().info("AuthMe 已检测到，启用 AuthMe 监听器...");
-            getServer().getPluginManager().registerEvents(new AuthMeListener(this), this);
-        } else {
-            getLogger().info("未检测到 AuthMe，启用常规监听器...");
-            getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-        }
+        getServer().getPluginManager().registerEvents(new PlayerEventListener(this, databaseManager), this);
+        getLogger().info("LongTimeNoSee 插件已启用！");
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("longtimenosee")) {
-            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                reloadConfig();
-                sender.sendMessage(ChatColor.GREEN + "配置已重新加载！");
-                return true;
-            }
+    public void onDisable() {
+        if (databaseManager != null) {
+            databaseManager.closeDatabase();
         }
-        return false;
+        getLogger().info("LongTimeNoSee 插件已禁用！");
+    }
+
+    public DatabaseManager getDatabaseManager() {
+        return databaseManager;
     }
 }
