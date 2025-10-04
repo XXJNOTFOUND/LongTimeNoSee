30674255095dfd64e3275c6097c49f09ee103be1
iXiangPro <132210248+XXJNOTFOUND@users.noreply.github.com>
2025-10-04 19:58:10 +0800

    Delete src directory

--- src/main/kotlin/com/ixiangpro/longtimenosee/utils/MessageUtils.kt
+++
@@ -1,9 +0,0 @@
-package com.ixiangpro.longtimenosee.utils
-
-import net.md_5.bungee.api.ChatColor
-
-object MessageUtils {
-    fun String.parseColors(): String {
-        return ChatColor.translateAlternateColorCodes('&', this)
-    }
-}
